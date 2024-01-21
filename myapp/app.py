ce8be847121d4ab37c734d5694a75e0032cc9308
Hyuga Hatanaka <hatakenakahyuga0707@gmail.com>
2024-01-21 16:20:47 +0900

    add new files

--- myapp/app.py
+++ myapp/app.py
@@ -1,7 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import calculate
 
 app = Flask(__name__)
 
 @app.route("/")
 def menu():
   return render_template("menu.html")
+
+#ベース
+@app.route("/calculate/", methods=["GET", "POST"])
+def cal():
+  if request.method=="POST":
+    ans = 0
+    var_a = 0
+    var_b = 0
+    var_a = request.form.get("var_a", type=int)
+    var_b = request.form.get("var_b", type=int)
+    if type(var_a) is not int:
+      var_a = 0
+    if type(var_b) is not int:
+      var_b = 0
+
+    mode = request.form.get("mode")
+    if mode == "plus":
+      ans = calculate.plus(var_a, var_b)
+    elif mode == "mlt_by":
+      ans = calculate.mlt_by(var_a, var_b)
+    
+    return render_template("calculate.html", ans=str(ans), var_a=str(var_a), var_b=str(var_b))
+  else:
+    return render_template("calculate.html")
+
+@app.route("/hello/", methods=["GET", "POST"])
+def hello():
+  return render_template("hello.html")
---
+++ myapp/calculate.py
@@ -0,0 +1,11 @@
+def plus(a, b):
+  return a + b
+
+def mlt_by(a, b):
+  return a * b
+
+def minus(a, b):
+  return a - b
+
+def div_by(a, b):
+  return round(a / b, 2)
